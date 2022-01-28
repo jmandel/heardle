52d19710c5e8444615f12f91f77486d063c66538
Josh Mandel <jmandel@med.harvard.edu>
2022-01-28 12:30:22 -0600

    Update target words

--- create-words-to-target.mjs
+++ create-words-to-target.mjs
@@ -1,15 +1,26 @@
 import fs from "fs";
+
 const words = JSON.parse(fs.readFileSync("words.json"));
 const probs = JSON.parse(fs.readFileSync("allprobs.json"));
 
-let wordsWithProbs = words.map(w => ({...w, ...(probs.find(p => p.word == w.word.toLowerCase()) || {p: 0})}));
+words.forEach(w => {w.word = w.word.toLowerCase()});
 
-let wordsToTarget = Object.values(wordsWithProbs.filter(w => w.p > .5).reduce((acc, w) => {
+const wordsIndexed = words.reduce((acc, w) => {
   acc[w.word] = acc[w.word] || [];
   acc[w.word].push(w);
   return acc;
-}, {})).filter(w => w.length == 1).map(w => w[0]);
+}, Object.create(null));
 
 
+const probsIndexed = probs.reduce((acc, w) => {
+  acc[w.word] = w;
+  return acc;
+});
 
-console.log(JSON.stringify(wordsToTarget, null, 2));
+let wordsWithProbs = Object.values(wordsIndexed)
+    .filter(w => w.length === 1)
+    .map(w => w[0])
+    .map(w => ({...w, ...(probsIndexed[w.word] || {p: 0})}))
+    .filter(w => w.phonemes.length === 5 && w.p > 0.5);
+
+console.log(JSON.stringify(wordsWithProbs, null, 2));
